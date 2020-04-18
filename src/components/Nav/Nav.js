60e8ab698b8461ee672e669d6712273d49812ab8
Gladys Alvarez <glad.alvarez1@gmail.com>
2020-04-18 15:24:50 -0500

    react router

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,21 +1,35 @@
 import React, { Component } from "react";
-import ('./navbar.css')
+import {getCharacters} from "../../services/characterService"
+import { Link } from "react-router-dom";
+import "./navbar.css";
 
 class Nav extends Component {
+state = {
+  results: [],
+};
+
+  componentDidMount() {
+   getCharacters().then((res) => {
+     const { results } = res.data;
+    this.setState({ results });
+   });
+  }
+
   render() {
     return (
-      <nav class="uk-navbar-container" uk-navbar="true">
-      <div class="uk-navbar-left">
+      <nav className="uk-navbar-container" uk-navbar="true">
+      <div className="uk-navbar-left">
   
-          <ul class="uk-navbar-nav">
-              <li class="uk-active"><a href="#">Active</a></li>
+          <ul className="uk-navbar-nav">
+              <li className="uk-active"><a href="#">Active</a></li>
               <li>
                   <a href="#">Parent</a>
-                  <div class="uk-navbar-dropdown">
-                      <ul class="uk-nav uk-navbar-dropdown-nav">
-                          <li class="uk-active"><a href="#">Active</a></li>
-                          <li><a href="#">Item</a></li>
-                          <li><a href="#">Item</a></li>
+                  <div className="uk-navbar-dropdown">
+                      <ul className="uk-nav uk-navbar-dropdown-nav">
+                         {this.state.results.map((item, index) => (
+                           <li key={index} className="uk-active">
+                           <Link to={`/detail/${item.id}`}>{item.name}</Link></li>
+                           ))}
                       </ul>
                   </div>
               </li>
