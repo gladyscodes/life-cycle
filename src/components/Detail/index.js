73d35ce79b14c6da3fe2a77512a9947062f0f148
Gladys Alvarez <glad.alvarez1@gmail.com>
2020-04-20 14:12:53 -0500

    example done

--- src/components/Detail/index.js
+++ src/components/Detail/index.js
@@ -1,9 +1,33 @@
 import React, { Component } from "react";
+import { getCharacter } from "../../services/characterService"
 
 class Detail extends Component {
+  state = {
+    character: {},
+  };
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    getCharacter(id).then((res) => {
+      console.log(res.data);
+      this.setState({ character: res.data });
+    })
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.match.params.id !== this.props.match.params.id) {
+      const { id } = nextProps.match.params;
+      getCharacter(id).then((res) => {
+        console.log(res.data);
+        this.setState({ character: res.data });
+      });
+    }
+  }
+
   render() {
-   console.log(this.props);
-      return <h1>Id: {this.props.match.params.id} </h1>;
+      return <div>
+        <h1>{this.state.character.name} </h1>;
+        <img src= {this.state.character.image} />
+      </div>
   }
 }
 
